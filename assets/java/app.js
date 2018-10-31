61c551117375583dd991f52d8ba9d07ed14e8f20
TheZShow <zgarcia72292@gmail.com>
2018-10-31 16:38:30 -0500

    added questions object

---
+++ assets/java/app.js
@@ -0,0 +1,50 @@
+var correctAnswers = 0;
+var wrongAnswer = 0;
+var countTimer = 30;
+var unanswered = 0;
+
+var trivQuestions = [{
+question: "Who is Spider-man?",
+answers: ["A literal spider in a suit", "George W. Bush", "Peter Parker", "A menace"],
+rightAnswer: "Peter Parker",
+image: ""
+},
+{
+question: "What was the name of Peter Parker's Uncle?",
+answers: ["Ben", "Benny", "Benny Benassi", "Papa Menace"],
+rightAnswer: "Ben",
+image: ""
+},
+{
+question: "Which artist performed the hit song 'Hero' for the film Spider-Man (2002) directed by Sam Raimi?",
+answers: ["Blink-182", "Chad Kroeger of Nickel Back", "Linkin Park", "The Menace Men"],
+rightAnswer: "Chad Kroeger of Nickel Back",
+image: ""    
+},
+{
+question: "When will Mr. Ditkovich, Peter's landlord, get his money?",
+answers: ["Every month on the 1st", "The second friday of every month", "When the NYPD finally catches the Spider-Menace", "When he fixes this damn door"],
+rightAnswer: "When he fixes this damn door",
+image: ""     
+},
+{
+question: "True or False: Norman Osbourne is something of a scientist",
+answers: ["True", "False"],
+rightAnswer: "True",
+image: ""
+},
+{
+question: "What does J. Jonah Jameson desire the most?",
+answers: ["Wealth", "Love", "For the Bugle to be the number 1 newspaper in New York", "Pictures of Spider-man"],
+rightAnswer:"Pictures of Spider-man",
+image: ""
+},
+{
+question: "Which professional wrestler played the character 'Bonesaw' from the film Spider-Man (2002) directed by Sam Raimi?",
+answers: ["'Macho Man' Randy Savage", "Hulk Hogan", "'Stone Cold' Steve Austin", "Rey Mysterio"],
+rightAnswer:"'Macho Man' Randy Savage",
+image: ""   
+}
+
+
+]
